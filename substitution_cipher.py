49420bb1d1a7d5ff996b214a3f11d1443b3465ac
RomainCuchet <47221720+RomainCuchet@users.noreply.github.com>
2022-06-09 09:54:06 +0200

    Create substitution_cipher.py

---
+++ substitution_cipher.py
@@ -0,0 +1,19 @@
+alphabet = [chr(97+i) for i in range(26)]
+print(alphabet)
+txt_file = ''
+def count(txt_file):
+    nb_letter = 0
+    dict = {}
+    txt_file = txt_file.lower()
+    for i in alphabet:
+        count = txt_file.count(i)
+        nb_letter += count
+        dict.update({i:count})
+    dict.update({'len':nb_letter})
+    return dict
+
+def print_insight(dict):
+    len = dict.get('len')
+    del dict['len']
+    for i in dict:
+        print(i + ': {} %'.format(round((dict.get(i)/len)*100),1))
